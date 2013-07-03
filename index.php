04408dbb5108a5f823f4466265bf188f94530ea7
megrez <lujiajing1126@163.com>
2013-07-03 13:00:50 +0800

    create the first version of the index page and upload other libs
    Change-Id: I2c24c2a661d5698629ba7de50b49ee646459c753

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<LINK rel="stylesheet" type="text/css" href="css/index.css" />
+<script type="text/javascript" src="js/lib/jquery-1.10.1.min.js"></script>
+<script type="text/javascript" src="js/index.js"></script>
+<title>测试页面alpha0.01</title>
+</head>
+<body>
+<div id="wrap">
+<div id="container">
+<div id="animate">
+<div id="title">
+<img src="img/alpha/miv5logo-white (1).png" />
+<BR>
+<a id="titleText">SAT TEST</a>
+</div>
+<div id="loginForm">
+<form id="Login">
+<input type="text" placeholder="账号"/>
+<p></p>
+<input type="password" placeholder="密码"/>
+</form>
+</div>
+</div>
+<div id="otherText">
+<ul id="loginText">
+<li>立即加入</li><li>忘记密码？</li>
+</ul>
+</div>
+</div>
+</div>
+</body>
+</html>
